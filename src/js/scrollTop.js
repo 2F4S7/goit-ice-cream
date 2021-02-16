afa6e2551a69588432a142c2b9f6e44b6f78d581
2F4S7 <72417264+2F4S7@users.noreply.github.com>
2021-02-16 03:47:24 +0200

    001

---
+++ src/js/scrollTop.js
@@ -0,0 +1,9 @@
+$('a[href^="#"]').click(function () {
+  var target = $(this).attr('href');
+  $('html, body').animate(
+    {
+      scrollTop: $(target).offset().top,
+    },
+    1000,
+  );
+});
